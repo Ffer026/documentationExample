d03c11b81e7e24f3c35f85b887fc6438a0b6b974
Fernando <fgaiper976@g.educaand.es>
2025-02-24 16:53:19 +0100

    Esto es un ejemplo de documentación incompleta en Java

--- Card.java
+++ Card.java
@@ -1,3 +1,10 @@
+
+/**
+ * Esta clase sirve para instanciar objetos carta, con las mismas propiedades que una carta normal de poker.
+ * 
+ * @author Francisco Fernando Gaitán Pérez
+ */
+
 public class Card {
     private final int rank;
     private final int suit;
@@ -22,11 +29,23 @@ public class Card {
         return s;
     }
 
+    /**
+     * equals
+     * Sirve para ver si ina carta es igual a otra
+     * 
+     * @param that El objeto carta con el que queremos comparar la carta actual
+     * @return Devuelve un booleano, true si son iguales, false si no
+     */
     public boolean equals(Card that) {
         return this.rank == that.rank
             && this.suit == that.suit;
     }
 
+    /**
+     * Ejemplo ejemplo
+     * @param that la otra carta
+     * @return Si es igual 0, menor -1 mayor 1
+     */
     public int compareTo(Card that) {
         if (this.suit < that.suit) {
             return -1;
@@ -48,6 +67,10 @@ public class Card {
         }
         return 0;
     }
+
+    /**
+     * bla bla
+     */
 
     public static void main(String[] args) {
         Card carta = new Card(1, 1);
